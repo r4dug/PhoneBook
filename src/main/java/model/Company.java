f5ecfd48b3c2363308ec93cf3eb09a79ba807e31
Radu Guran <radug314@gmail.com>
2020-09-20 18:45:44 +0300

    Added fields in Company class

--- src/main/java/model/Company.java
+++ src/main/java/model/Company.java
@@ -1,4 +1,32 @@
 package model;
 
 public class Company {
+
+    private String name;
+    private String jobTitle;
+
+    public Company(String name) {
+        this.name = name;
+    }
+
+    public Company(String name, String jobTitle) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
 }
